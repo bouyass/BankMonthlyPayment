a58830703c46d9cbfa926c83de76260a183fea33
lyes <lyes.makhloufi@outlook.fr>
2020-06-17 11:49:41 +0200

    HTML to JSP

---
+++ src/controllers/CalculatorController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CalculatorController extends HttpServlet {
+	
+	@Override 
+	public void init() throws ServletException{
+		
+	}
+	
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException{
+		
+		
+		
+	}
+
+}
